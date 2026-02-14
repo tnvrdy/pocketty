fdc30423d77cbee62ebfa56acac479bb0f63a1b2
tnvrdy <tanvired@stanford.edu>
2026-02-14 02:55:43 -0800

    fix

--- pocketty/src/core.rs
+++
@@ -1,9 +0,0 @@
-use crate::shared::UiAction;
-use crate::audio_api::{AudioCommand, TriggerParams};
-
-pub fn action_to_audio(action: UiAction) -> Option<AudioCommand> {
-    match action {
-        UiAction::PadDown(pad) => Some(AudioCommand::Trigger(TriggerParams { pad })),
-        _ => None,
-    }
-}
