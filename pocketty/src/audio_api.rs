8ca43fdce2e253e64a3426ce9372a4546f3a9090
Ryan McCormick <ryan.m.mccormick2@gmail.com>
2026-02-14 05:08:44 -0800

    update api design to reflect new laoder logic

--- pocketty/src/audio_api.rs
+++ pocketty/src/audio_api.rs
@@ -1,11 +1,26 @@
-use crate::shared::PadId;
+use crate::audio::effect::EffectSpec;
+use crate::audio::sample_buffer::SampleBuffer;
+use crate::audio::SampleId;
 
-#[derive(Clone, Copy, Debug)]
+pub use crate::audio::SampleId;
+
+#[derive(Clone, Debug)]
 pub struct TriggerParams {
-    pub pad: PadId,
+    pub sample_id: SampleId,
+    pub trim_start: usize,
+    pub length: usize,
+    pub gain: f32,
+    pub pitch: f32,
+    pub effect_chain: Vec<EffectSpec>,
 }
 
 #[derive(Clone, Debug)]
 pub enum AudioCommand {
+    // The engine can't load files (interrupts thread), so we you must first 
+    // register a preloaded buffer (see sample_loader.rs), then send that to 
+    // the engine
+    RegisterSample { id: SampleId, buffer: SampleBuffer },  
+    
+    // The engine then uses the sample id to trigger the sound 
     Trigger(TriggerParams),
 }
