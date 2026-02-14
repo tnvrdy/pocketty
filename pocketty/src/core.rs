da6f774a24d3b83fde65c6aeaafd192ae5e9b13e
Ryan McCormick <ryan.m.mccormick2@gmail.com>
2026-02-14 02:45:37 -0800

    remove errant velocity param

--- pocketty/src/core.rs
+++ pocketty/src/core.rs
@@ -3,7 +3,7 @@ use crate::audio_api::{AudioCommand, TriggerParams};
 
 pub fn action_to_audio(action: UiAction) -> Option<AudioCommand> {
     match action {
-        UiAction::PadDown(pad) => Some(AudioCommand::Trigger(TriggerParams { pad, velocity: 1.0 })),
+        UiAction::PadDown(pad) => Some(AudioCommand::Trigger(TriggerParams { pad })),
         _ => None,
     }
 }
